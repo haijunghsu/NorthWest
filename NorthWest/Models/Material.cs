fad94729d1e97a1cd2cd4fc659a97b2c7f7d72cf
haijunghsu <haironghsu@gmail.com>
2018-12-05 16:17:34 -0700

    Assay model

---
+++ NorthWest/Models/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthWest.Models
+{
+    [Table("Material")]
+    public class Material
+    {
+        [Key]
+        public int MaterialID { get; set; }
+        public string MaterialName { get; set; }
+    }
+}

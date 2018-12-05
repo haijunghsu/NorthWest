10ff3f2e0db1cfc31a3645564cc4bb8112e7758d
tiaramj <tiaramj@users.noreply.github.com>
2018-12-05 16:26:59 -0700

    Test.cs

---
+++ NorthWest/Models/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthWest.Models
+{
+    [Table("Test")]
+    public class Test
+    {
+        [Key]
+        public int TestID { get; set; }
+        public int AssayID { get; set; }
+        public string TestName { get; set; }
+        public int BasePrice { get; set; } 
+        public string TestRequired { get; set; }
+    }
+}
